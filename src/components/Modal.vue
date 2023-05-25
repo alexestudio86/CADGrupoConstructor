a29ab0451804a5de58e4a8138794c9b9837b4aa2
Alejandro R <alexestudio86@hotmail.com>
2023-05-24 21:22:14 -0600

    Final version 1.0

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,11 @@
 <template>
     <div ref="modal" class="w3-modal w3-black w3-show" style="z-index: 2;">
-        <span @click="handleCloseModal" class="w3-large w3-white w3-button w3-display-topright" title="Cerrar">x</span>
+        <button @click="$event => handleCloseModal( modalElements )" class="w3-large w3-white w3-button w3-display-topright" title="Cerrar">x</button>
         <figure class="w3-display-middle w-100 w3-content">
             <img class="modal-image" :src="images[modalElements.idx].source" :alt="images[modalElements.idx].title" />
-            <button class="w3-button w3-gray w3-display-left" @click="$event => handlePrevImage( modalElements )">❮</button>
-            <button class="w3-button w3-gray w3-display-right" @click="$event => handleNextImage( modalElements )">❯</button>
-            <figcaption class="w3-black w3-opacity w3-large w3-padding-small w3-display-bottommiddle w-100 w3-center">{{ allProps.images[modalElements.idx].source }} - {{ modalElements.idx }}</figcaption>
+            <button v-if="modalElements.idx > 0" class="w3-button w3-gray w3-display-left" @click="$event => handlePrevImage( images, modalElements )">❮</button>
+            <button v-if="modalElements.idx < images.length-1" class="w3-button w3-gray w3-display-right" @click="$event => handleNextImage( images, modalElements )">❯</button>
+            <figcaption class="w3-black w3-opacity w3-large w3-padding-small w3-display-bottommiddle w-100 w3-center">{{ allProps.images[modalElements.idx].title }}</figcaption>
         </figure>
     </div>
 </template>
@@ -20,22 +20,20 @@
         }
     );
 
-    const handleCloseModal = ( ) => {
-        console.log('Closed')
+    const handleCloseModal = ( modalElements ) => {
+        modalElements.status = false
     };
 
-    const handlePrevImage = ( evt ) => {
-        evt.idx--
-    };
-
-    const handleNextImage = ( evt ) => {
-        console.log( images )
-        if ( evt.idx < images.length ) {
-            console.log( 'Si pasa' )
-        } else {
-            console.log( 'No pasa' )
+    const handlePrevImage = ( images, modalElements ) => {
+        if( modalElements.idx > 0 ){
+            modalElements.idx--;
         }
-        //evt.idx++
+    };
+
+    const handleNextImage = ( images, modalElements ) => {
+        if( modalElements.idx < images.length-1 ){
+            modalElements.idx++;
+        }
     };
 
 
